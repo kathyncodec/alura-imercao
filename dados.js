9919813fde2be4cace17e8c1f98a7f36ded9f5a6
Katharyn <111580312+kathyncodec@users.noreply.github.com>
2024-09-09 13:53:07 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,19 @@
+let dados = [
+  {
+    titulo: "O Homem Invisível",
+    descricao: "O Homem Invisível, de H.G. Wells, é um clássico da ficção científica que explora os limites da ciência e as consequências de mexer com forças da natureza. A história acompanha um cientista que, obcecado pela invisibilidade, desenvolve um soro que o torna transparente. No entanto, à medida que se afasta da sociedade e se isola, a invisibilidade o corrompe, revelando sua verdadeira natureza e levando-o à loucura. A obra de Wells não apenas antecipou muitos dos temas explorados na ficção científica moderna, como também serve como uma profunda reflexão sobre os perigos da ambição desmedida e a importância da conexão humana.",
+    link:"https://www.amazon.com.br/Homem-Invis%C3%ADvel-H-G-Wells/dp/8537815861/ref=sr_1_2?dib=eyJ2IjoiMSJ9.zNL_iigAiPW83OYSwJXS7SbkS8Ap-Nsoc7oU6EuyIPO5CM0zT5JM2608r9DrwKLOdqvDFVz_zSF9q247BJYANaR4-0Z0ihrY4Sc5jXFFMhQy5CeTNBGqFWqLTj_CeATZvcx_nOx_0xSJxGPI0uHJkUXH3qMK7KphzBkT3_FSvLOlhY0_JRgSWz220AM504re413Piwitpu1NPHZPgByW28jcvF5azk-DY1eEN5Dnm_0qEj7Gb4ujB7QWgkxro9b_KldtgJID6IUIGj6upVoSb7RZCTYJ-SxEARWkKjFkuUs.YnYWbMg-s3lKLzahM5-_lrVWv_yCXDkomXG-G3itBx0&dib_tag=se&keywords=o+homem+invis%C3%ADvel&qid=1725366106&sr=8-2",
+    tegs: "homem invisibilidade h.g.wells"
+  },
+  
+ {
+    titulo: "Como Eu Era Antes de Você",
+    descricao: "Louisa Clark não tem grandes ambições, mas precisa de um emprego. Ao se tornar cuidadora de Will Traynor, um jovem rico e sarcástico que sofreu um acidente, sua vida muda completamente. Juntos, eles embarcam em uma jornada emocionante que os levará a questionar o valor da vida e a importância dos relacionamentos.",
+    link: "https://www.amazon.com.br/Como-Eu-Era-Antes-Voc%C3%AA/dp/8580573297/ref=sr_1_2?dib=eyJ2IjoiMSJ9.NWQCwgIUdq8IcQu8bOy1CZB0KX-3Cv_gdbsRbqQH-3vNsyKjqkagpT3lvGmBtOP0upzCyS4rKjI5AFb3iwgzc8sapRa2esHeR-Ltv3hiAJezKey3LFyB4sS20s1-NlHsgOy-Qp8PI1QlpWhCHKKt-IkIZSTboDeEACDzcbGN7OhYGrtEg582boVt6N_b_bcdC9RfsXpPx3cvBuTFjaf9HaaiDmpSh1nW3L7jVLuxMctxbD3TO79wfBlRJf7iDeScvnNZa6_WThz9L4lCOIYM2Rr9lrDddVr7hv31cdVij_Q.DB7yC4pntIPLVJhvl1bPBR0DqvNc1w2s2e1jVh2ApJQ&dib_tag=se&keywords=como+eu+era+antes+de+voce&qid=1725883745&sr=8-2",
+  },
+  
+{
+    titulo: " Quatro Vidas de um Cachorro "
+    descricao: "Uma história emocionante e comovente sobre a lealdade e o amor incondicional entre um homem e seu cachorro. Acompanhe as quatro vidas de um cão que reencarna em diferentes épocas, sempre buscando o seu dono e aprendendo lições valiosas sobre a vida."
+    link: "https://www.amazon.com.br/Quatro-Vidas-Um-Cachorro-Filme/dp/8569514719/ref=sr_1_1?crid=B7UWU4TZQ3DO&dib=eyJ2IjoiMSJ9.6ur_pXWjwEcUlA-MvZE4QiH2uKmjz9ypiQwjNGYfU4fQABQKgPCigfW1ETOGPTnXOoLW2afnUWzib22ypbcpQi546xWpUD0URxhYZmgLOvfLtTi3GMV1KOcGKX0g0CSs5A9gfkuyeWitSkkTSQSBcMVPebX8P_rwUkWLwI5UqPdHw6E5D_Qj4bV8XpZaUCII63NRIDN1eZn0F0ZRtHEnbWsi2skO-QgG8ZxzfG8-_dK8I51dpWWu1M1Ru2ko935cY6mbWrwON6-qPhm8h5WfmLwo_htuYD-qHY9m3n4MMXM.4nOPRNN4yckqR1jt3bDXvBudpIqYbtZmVfqflAjyL24&dib_tag=se&keywords=quatro+vidas+de+um+cachorros+livro&qid=1725883769&sprefix=quatr%2Caps%2C186&sr=8-1"
+  } ];
